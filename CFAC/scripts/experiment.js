b7e9e7b54072921fb1425d804cdb03bd5d2eaa3d
thedailies <tclark@zonoff.com>
2015-03-03 10:19:20 -0500

    test typekit

---
+++ CFAC/scripts/experiment.js
@@ -0,0 +1,45 @@
+/**
+ * Copyright © 2015 Zonoff, Inc.  All Rights Resreved.
+ */
+
+'use strict';
+
+(function(global) {
+
+  var app = angular.module('app', []);
+
+  app.factory('dataService', ['$http', '$q', function($http, $q) {
+
+    var items = [];
+
+    $http.get('data/data.json').then(function (response) {
+      response.data.forEach(function (element) {
+        items.push(element);
+      });
+    });
+
+    var fetchNewPair = function() {
+      var random = Math.floor(Math.random() * items.length);
+      return items[random];
+    };
+
+    return {
+      fetch: fetchNewPair
+    };
+
+  }]);
+
+  app.controller('randomizer', ['$scope', 'dataService', function($scope, dataService) {
+
+    $scope.likes = "thing one";
+    $scope.dislikes = "thing";
+
+    $scope.refresh = function() {
+      var item = dataService.fetch();
+      $scope.likes = item.likes;
+      $scope.dislikes = item.dislikes;
+    }
+  }]);
+
+
+})(this);
